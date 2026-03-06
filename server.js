0163117546276fd5a7c0373e6d6e543fdfb9b249
Luciano Dittgen <luciano@mindfieldconsulting.com>
2026-03-06 15:38:12 -0300

    feat: implementa autenticacao JWT
    Cria middleware de seguranca para proteger as rotas do CRUD e adiciona endpoint de login para geracao de token.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const authMiddleware = require("./middlewares/authMiddleware");
 const express = require("express");
 const mongoose = require("mongoose");
 const orderController = require("./controllers/orderController");
@@ -15,12 +17,19 @@ app.use(express.json());
 // Rota da Documentação (Swagger)
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.post("/login", (req, res) => {
+	const token = jwt.sign({ user: "admin_jitterbit" }, process.env.JWT_SECRET, {
+		expiresIn: "1h",
+	});
+	res.json({ token });
+});
+
 // Rotas da API
-app.post("/order", orderController.createOrder);
-app.get("/order/list", orderController.listOrders);
-app.get("/order/:id", orderController.getOrderById);
-app.put("/order/:id", orderController.updateOrder);
-app.delete("/order/:id", orderController.deleteOrder);
+app.post("/order", authMiddleware, orderController.createOrder);
+app.get("/order/list", authMiddleware, orderController.listOrders);
+app.get("/order/:id", authMiddleware, orderController.getOrderById);
+app.put("/order/:id", authMiddleware, orderController.updateOrder);
+app.delete("/order/:id", authMiddleware, orderController.deleteOrder);
 
 // Configurações de Ambiente
 const PORT = process.env.PORT || 3000;
